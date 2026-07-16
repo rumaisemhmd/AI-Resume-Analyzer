3c632c136f0106a7fbe67475860feb861e05115d
Mohammed Rumaise A <mhmdrumaise@gmail.com>
2026-07-17 00:40:19 +0530

    fix missing os import

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,7 +118,7 @@ USE_TZ = True
 
 
 
-import os
+
 
 STATIC_URL = 'static/'
 
